9a47ddd2c5cc479d07c309227bacb5696dd0dfa1
AlexeyVasilev <alex89v@gmail.com>
2026-03-29 18:49:20 +0500

    Improve TLS Stream parsing with reassembly-backed record reconstruction

--- tests/unit/StreamQueryTests.cpp
+++ tests/unit/StreamQueryTests.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <string>
 #include <vector>
 
@@ -229,6 +230,121 @@ void run_stream_query_tests() {
     PFL_EXPECT(split_tls_rows[0].packet_indices == expected_split_packet_indices);
     PFL_EXPECT(split_tls_rows[0].protocol_text.find("Handshake Type: ServerHello") != std::string::npos);
     PFL_EXPECT(!split_tls_rows[0].payload_hex_text.empty());
+
+    const auto split_app_data_record = make_tls_record(0x17U, 0x0303U, {0xDEU, 0xADU, 0xBEU, 0xEFU, 0x11U, 0x22U});
+    const auto split_app_payload_a = std::vector<std::uint8_t>(
+        split_app_data_record.begin(),
+        split_app_data_record.begin() + 6
+    );
+    const auto split_app_payload_b = std::vector<std::uint8_t>(
+        split_app_data_record.begin() + 6,
+        split_app_data_record.end()
+    );
+    const auto split_app_packet_a = make_ethernet_ipv4_tcp_packet_with_bytes_payload(
+        ipv4(10, 46, 0, 1), ipv4(10, 46, 0, 2), 52004, 443, split_app_payload_a, 0x18);
+    const auto split_app_packet_b = make_ethernet_ipv4_tcp_packet_with_bytes_payload(
+        ipv4(10, 46, 0, 1), ipv4(10, 46, 0, 2), 52004, 443, split_app_payload_b, 0x18);
+    const auto split_app_path = write_temp_pcap(
+        "pfl_stream_query_tls_split_appdata.pcap",
+        make_classic_pcap({
+            {100, split_app_packet_a},
+            {200, split_app_packet_b},
+        })
+    );
+
+    CaptureSession split_app_session {};
+    PFL_EXPECT(split_app_session.open_capture(split_app_path, fast_options));
+    const auto split_app_rows = split_app_session.list_flow_stream_items(0);
+    PFL_EXPECT(split_app_rows.size() == 1);
+    PFL_EXPECT(split_app_rows[0].label == "TLS AppData");
+    PFL_EXPECT(split_app_rows[0].byte_count == split_app_data_record.size());
+    PFL_EXPECT(split_app_rows[0].packet_count == 2);
+    PFL_EXPECT(split_app_rows[0].packet_indices == expected_split_packet_indices);
+    PFL_EXPECT(split_app_rows[0].protocol_text.find("Record Type: ApplicationData") != std::string::npos);
+    PFL_EXPECT(!split_app_rows[0].payload_hex_text.empty());
+
+    const auto multi_record_server_hello = make_tls_handshake_record(0x02U, {0x10U, 0x11U, 0x12U, 0x13U});
+    const auto multi_record_ccs = make_tls_change_cipher_spec_record();
+    const auto multi_record_app_data = make_tls_record(0x17U, 0x0303U, {0x21U, 0x22U, 0x23U, 0x24U, 0x25U});
+    const auto multi_record_payload = concat_bytes(
+        concat_bytes(multi_record_server_hello, multi_record_ccs),
+        multi_record_app_data
+    );
+    const auto multi_record_split = static_cast<std::ptrdiff_t>(multi_record_server_hello.size() + 2U);
+    const auto multi_record_payload_a = std::vector<std::uint8_t>(
+        multi_record_payload.begin(),
+        multi_record_payload.begin() + multi_record_split
+    );
+    const auto multi_record_payload_b = std::vector<std::uint8_t>(
+        multi_record_payload.begin() + multi_record_split,
+        multi_record_payload.end()
+    );
+    const auto multi_record_packet_a = make_ethernet_ipv4_tcp_packet_with_bytes_payload(
+        ipv4(10, 47, 0, 1), ipv4(10, 47, 0, 2), 52005, 443, multi_record_payload_a, 0x18);
+    const auto multi_record_packet_b = make_ethernet_ipv4_tcp_packet_with_bytes_payload(
+        ipv4(10, 47, 0, 1), ipv4(10, 47, 0, 2), 52005, 443, multi_record_payload_b, 0x18);
+    const auto multi_record_path = write_temp_pcap(
+        "pfl_stream_query_tls_reassembled_sequence.pcap",
+        make_classic_pcap({
+            {100, multi_record_packet_a},
+            {200, multi_record_packet_b},
+        })
+    );
+
+    CaptureSession multi_record_session {};
+    PFL_EXPECT(multi_record_session.open_capture(multi_record_path, fast_options));
+    const auto multi_record_rows = multi_record_session.list_flow_stream_items(0);
+    PFL_EXPECT(multi_record_rows.size() == 3);
+    PFL_EXPECT(multi_record_rows[0].label == "TLS ServerHello");
+    PFL_EXPECT(multi_record_rows[1].label == "TLS ChangeCipherSpec");
+    PFL_EXPECT(multi_record_rows[2].label == "TLS AppData");
+    PFL_EXPECT(multi_record_rows[0].byte_count == multi_record_server_hello.size());
+    PFL_EXPECT(multi_record_rows[1].byte_count == multi_record_ccs.size());
+    PFL_EXPECT(multi_record_rows[2].byte_count == multi_record_app_data.size());
+    PFL_EXPECT(multi_record_rows[0].packet_indices == std::vector<std::uint64_t> {0});
+    PFL_EXPECT(multi_record_rows[1].packet_indices == expected_split_packet_indices);
+    PFL_EXPECT(multi_record_rows[2].packet_indices == std::vector<std::uint64_t> {1});
+    PFL_EXPECT(multi_record_rows[0].protocol_text.find("Handshake Type: ServerHello") != std::string::npos);
+    PFL_EXPECT(multi_record_rows[1].protocol_text.find("Record Type: ChangeCipherSpec") != std::string::npos);
+    PFL_EXPECT(multi_record_rows[2].protocol_text.find("Record Type: ApplicationData") != std::string::npos);
+
+    const auto incomplete_reassembled_app_data = std::vector<std::uint8_t> {
+        0x17U, 0x03U, 0x03U, 0x00U, 0x04U, 0xAAU, 0xBBU,
+    };
+    const auto reassembled_partial_payload = concat_bytes(server_hello_record, incomplete_reassembled_app_data);
+    const auto reassembled_partial_split = static_cast<std::ptrdiff_t>(server_hello_record.size() + 2U);
+    const auto reassembled_partial_payload_a = std::vector<std::uint8_t>(
+        reassembled_partial_payload.begin(),
+        reassembled_partial_payload.begin() + reassembled_partial_split
+    );
+    const auto reassembled_partial_payload_b = std::vector<std::uint8_t>(
+        reassembled_partial_payload.begin() + reassembled_partial_split,
+        reassembled_partial_payload.end()
+    );
+    const auto reassembled_partial_packet_a = make_ethernet_ipv4_tcp_packet_with_bytes_payload(
+        ipv4(10, 48, 0, 1), ipv4(10, 48, 0, 2), 52006, 443, reassembled_partial_payload_a, 0x18);
+    const auto reassembled_partial_packet_b = make_ethernet_ipv4_tcp_packet_with_bytes_payload(
+        ipv4(10, 48, 0, 1), ipv4(10, 48, 0, 2), 52006, 443, reassembled_partial_payload_b, 0x18);
+    const auto reassembled_partial_path = write_temp_pcap(
+        "pfl_stream_query_tls_reassembled_partial.pcap",
+        make_classic_pcap({
+            {100, reassembled_partial_packet_a},
+            {200, reassembled_partial_packet_b},
+        })
+    );
+
+    CaptureSession reassembled_partial_session {};
+    PFL_EXPECT(reassembled_partial_session.open_capture(reassembled_partial_path, fast_options));
+    const auto reassembled_partial_rows = reassembled_partial_session.list_flow_stream_items(0);
+    PFL_EXPECT(reassembled_partial_rows.size() == 2);
+    PFL_EXPECT(reassembled_partial_rows[0].label == "TLS ServerHello");
+    PFL_EXPECT(reassembled_partial_rows[1].label == "TLS Record Fragment");
+    PFL_EXPECT(reassembled_partial_rows[1].packet_indices == expected_split_packet_indices);
+    PFL_EXPECT(reassembled_partial_rows[1].protocol_text.find("do not contain a complete TLS record") != std::string::npos);
+    PFL_EXPECT(reassembled_partial_rows[1].protocol_text.find("ApplicationData") == std::string::npos);
 }
 
 }  // namespace pfl::tests
+
+
+
